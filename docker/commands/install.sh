61cc49b9e301244041a65f876ade222fcab12fa6
William Correa <wilcorrea@gmail.com>
2018-01-13 20:46:13 -0200

    Installer

--- docker/commands/install.sh
+++ docker/commands/install.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
+# globals
 DOCKER="${1}/docker"
-ENABLED="${DOCKER}/enabled"
 TEMPLATE="${DOCKER}/template"
 
+# resolve the main settings
+ENABLED="${DOCKER}/enabled"
 mkdir -p ${ENABLED}
 touch "${ENABLED}/${2}"
 
+# create dir to receive the app
 SOURCE="${1}/${2}/app"
-REPO="${1}/${2}/app.git"
-TEMP="${1}/${2}/temp"
-
 mkdir -p ${SOURCE}
-mkdir -p ${REPO}
-mkdir -p ${TEMP}
 
+# create and configure the bare repo
+REPO="${1}/${2}/app.git"
+mkdir -p ${REPO}
 cd ${REPO}
 git init --bare
+touch "hooks/post-receive"
+chmod +x "hooks/post-receive"
 
-touch "${REPO}/hooks/post-receive"
-
+# create a temp repo to push first changes
+TEMP="${1}/${2}/temp"
+mkdir -p ${TEMP}
 cd ${TEMP}
 git init && git remote add origin ${REPO}
 touch "index.html" "docker-compose.yml"
